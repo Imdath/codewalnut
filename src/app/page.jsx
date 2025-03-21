97993fb72b7ae62f1debc875e221ca9d391342fc
Imdath <imdathmohamed@gmail.com>
2025-03-21 19:26:22 +0530

    code: built the search field, fetched the pokemon data and displayed them

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,41 @@
 /* eslint-disable react/jsx-filename-extension */
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { fetchPokemons, handleSearch } from "./functions";
+import PokemonCard from "@/components/PokemonCard";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-2 row-start-2 items-center justify-center">
-        <Image
-          src="/images/codewalnut-logo.svg"
-          alt="CodeWalnut logo"
-          width={180}
-          height={38}
-          priority
-        />
+  const [search, setSearch] = useState("");
+  const [pokemons, setPokemons] = useState(null);
 
-        <h1 className="text-4xl font-bold mt-6">Tech Test</h1>
-        <h2 className="text-lg">Good luck!</h2>
+  useEffect(() => {
+    fetchPokemons(setPokemons, search);
+  }, [search]);
+
+  return (
+    <div className="grid grid-rows-[auto_1fr_20px] items-start justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      {/* Search Pokemon Input Field */}
+      <header className="w-full flex justify-center">
+        <input
+          id="search"
+          type="search"
+          name="search"
+          className="border border-gray-400 p-2 rounded-md text-black placeholder-gray-500 bg-white w-full max-w-md"
+          value={search}
+          onChange={(e) => handleSearch(e, setSearch)}
+          aria-label="Search Pokemon"
+          placeholder="Search Pokemon by name or id"
+          autoComplete="off"
+        />
+      </header>
+      {/* Pokemon Cards Container */}
+      <main className="flex flex-col gap-6 items-center justify-center p-8">
+        <div className="grid grid-cols-1 min-[420px]:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+          {pokemons?.map((pokemon) => {
+            return <PokemonCard pokemon={pokemon} />;
+          })}
+        </div>
       </main>
     </div>
   );
